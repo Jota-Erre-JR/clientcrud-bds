9e574b890845c0e64789201afc1c2f51c19a4cc5
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-08-02 21:13:13 -0300

    Backend project created

---
+++ backend/src/main/java/com/jotaerre/clientCrud/ClientCrudApplication.java
@@ -0,0 +1,13 @@
+package com.jotaerre.clientCrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCrudApplication.class, args);
+	}
+
+}

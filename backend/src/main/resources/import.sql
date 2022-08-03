2b577122978601471425c4f9cd056c453d3040ca
Jota-Erre-JR <juliocesarjr@outlook.com>
2022-08-03 02:19:12 -0300

    Client entity, .properties configurations, pom dependencies, SQL script.

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Nelson Mandela', '233456789', 20000.0, DATE '1985-10-21', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Isaac Newton', '233456790', 13000.0, DATE '1993-01-22', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Drummond de Andrade', '233456791', 25000.0, DATE '1978-04-23', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernando Pessoa', '233456792', 18000.0, DATE '1985-02-24', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Monteiro Lobato', '233456793', 23500.0, DATE '1975-02-08', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Anne Frank', '233456794', 56000.0, DATE '1974-12-21', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Sofia Ionescu', '233456795', 48000.0, DATE '1948-01-16', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Valentina Tereshkova', '233456796', 12000.0, DATE '1954-07-13', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Malala Yousafzai', '233456797', 21000.0, DATE '1984-11-15', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Marie Curie', '233456798', 50000.0, DATE '1986-06-26', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Greta Thunberg', '233456799', 20000.0, DATE '1986-10-21', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Napoleon Hill', '233456800', 156000.0, DATE '1994-01-22', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Emily Dickinson', '233456801', 22000.0, DATE '1979-04-23', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Tarsila do Amaral', '233456802', 100000.0, DATE '1986-02-24', 1);
+
